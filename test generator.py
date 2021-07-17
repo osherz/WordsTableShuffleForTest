e6129a1ba2fea532d424a3800ef5fa1a34e49ac7
Osher Zakin <osherz92@gmail.com>
2021-07-17 20:46:59 +0300

    Extract the paragraph replaces to method

--- test generator.py
+++ test generator.py
@@ -4,11 +4,30 @@ import random
 FILENAME = 'תמצית הקורס.docx'
 TEST_FILENAME = 'test.docx'
 
-doc = Document(docx=FILENAME)
-for table in doc.tables:
-    for row in table.rows:
+
+def main():
+    doc = Document(docx=FILENAME)
+    for table in doc.tables:
+        replace_random_paragraphs_from_table(table)
+    doc.save(TEST_FILENAME)
+
+
+def replace_random_paragraphs_from_table(table, paragraph_replace_precent=0.5, replace_with='0'):
+    """
+    Replace random paragraph from table with alternative text.
+
+    :param table:
+    :param replace_with:
+    :return:
+    """
+    MAX_NUMBER_TO_RANDOM = 6
+    # To not include the header
+    for row in table.rows[1:]:
         for cell in row.cells:
             for paragraph in cell.paragraphs:
-                if random.randrange(0, 6) > 3:
-                    paragraph.text = '0'
-doc.save(TEST_FILENAME)
+                if random.randrange(0, MAX_NUMBER_TO_RANDOM) > MAX_NUMBER_TO_RANDOM * paragraph_replace_precent:
+                    paragraph.text = replace_with
+
+
+if __name__ == '__main__':
+    main()
