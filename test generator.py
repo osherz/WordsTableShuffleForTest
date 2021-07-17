d96f07ef7125b727b104a4666fc4f3f653577296
Osher Zakin <osherz92@gmail.com>
2021-07-17 20:57:10 +0300

    Add function to shuffle (still not working)

--- test generator.py
+++ test generator.py
@@ -1,3 +1,4 @@
+import docx
 from docx.api import Document
 import random
 
@@ -9,6 +10,7 @@ def main():
     doc = Document(docx=FILENAME)
     for table in doc.tables:
         replace_random_paragraphs_from_table(table)
+        shuffle_table_rows(table)
     doc.save(TEST_FILENAME)
 
 
@@ -27,6 +29,19 @@ def replace_random_paragraphs_from_table(table, paragraph_replace_precent=0.5, r
             for paragraph in cell.paragraphs:
                 if random.randrange(0, MAX_NUMBER_TO_RANDOM) > MAX_NUMBER_TO_RANDOM * paragraph_replace_precent:
                     paragraph.text = replace_with
+
+
+# Still not working. Invoke error that Table.roww 'can't set attribute'
+def shuffle_table_rows(table: docx.Table):
+    """
+    Shuffle the table rows.
+    :param table:
+    :return:
+    """
+    rows_to_shuffle = table.rows[1:]
+    random.shuffle(rows_to_shuffle)
+    # Be aware to save the first row as header
+    table.rows = table.rows[0:1] + rows_to_shuffle
 
 
 if __name__ == '__main__':
